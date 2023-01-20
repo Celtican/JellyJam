4b2bbd98b74822cab5aa0ce69602033d8e4665e7
CoderValencia <valencia.coleman625@gmail.com>
2023-01-20 18:50:32 -0500

    Test Script by Valencia

---
+++ Assets/Scripts/VcTestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VcTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("A wild Valencia appeared. ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
