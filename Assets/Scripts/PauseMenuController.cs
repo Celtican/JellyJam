67962a66426e63a8d5eebe3b3f10d0694c097181
Celtican <Celtican5@gmail.com>
2023-01-20 23:57:30 -0500

    Added pause menu prefab

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused()) Unpause();
+            else Pause();
+        }
+    }
+    
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Unpause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public bool IsPaused()
+    {
+        return pauseMenu.activeInHierarchy;
+    }
+
+    public void SetScene(string sceneName)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneName);
+    }
+}
