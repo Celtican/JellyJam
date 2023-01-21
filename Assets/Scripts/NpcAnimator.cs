e6d2216ad52775e2b1f4b5c9bfff337208ee9fae
Celtican <Celtican5@gmail.com>
2023-01-21 18:00:59 -0500

    Added NPC animation and voice

---
+++ Assets/Scripts/NpcAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NpcAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private static readonly int Talking = Animator.StringToHash("Talking");
+
+    public UnityEvent onStopWalking;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void StartTalking()
+    {
+        animator.SetBool(Talking, true);
+    }
+    public void StopTalking()
+    {
+        animator.SetBool(Talking, false);
+    }
+
+    public void StopWalking() // called by animator
+    {
+        onStopWalking.Invoke();
+    }
+}
