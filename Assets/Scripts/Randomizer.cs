9bad5b50aea31b41d47dea831faf2f3bbb778c6b
CoderValencia <valencia.coleman625@gmail.com>
2023-01-21 19:30:43 -0500

    Added Character Randomizer

--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -5,34 +5,19 @@ using UnityEngine;
 public class Randomizer : MonoBehaviour
 {
     private SpriteRenderer rend;
-    private int headSpriteNum;
-    public Sprite currentHeadSprite;
-    public Sprite[] headSprites;
+
+
+    public Sprite[] spriteList;
 
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
-        currentHeadSprite = headSprites[0];
-        
+        GetRandomSprite();  
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetRandomSprite()
     {
-        
-    }
-
-    public void ChangeHeadSprite()
-    {
-        headSpriteNum = Random.Range(0, headSprites.Length);
-        Debug.Log(headSpriteNum);
-        currentHeadSprite = headSprites[headSpriteNum];
-        rend.sprite = currentHeadSprite;
-    }
-
-    public void ChangeEyeSprite()
-    {
-        
+        rend.sprite = spriteList[Random.Range(0, spriteList.Length)];
     }
 }
