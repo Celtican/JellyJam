5ce3006c686875f9f1d4feb990503b3b220faf59
CoderValencia <valencia.coleman625@gmail.com>
2023-01-21 22:43:31 -0500

    Added Color to Randomizer

--- Assets/Scripts/PlayerTraits.cs
+++ Assets/Scripts/PlayerTraits.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VcTestScript : MonoBehaviour
+public class PlayerTraits : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("A wild Valencia appeared. ");
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Randomizer : MonoBehaviour
 {
     private SpriteRenderer rend;
-
-
     public Sprite[] spriteList;
+
+    public Color[] colorList;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +19,9 @@ public class Randomizer : MonoBehaviour
     public void GetRandomSprite()
     {
         rend.sprite = spriteList[Random.Range(0, spriteList.Length)];
+        rend.color = colorList[Random.Range(0, colorList.Length)];
+
     }
+
+
 }
