d984fa630f6b29a9b51a93b94d13415f14534f47
CoderValencia <valencia.coleman625@gmail.com>
2023-01-22 23:22:15 -0500

    Updated generator on player preference

--- Assets/Scripts/CharacterGeneratorNPC.cs
+++ Assets/Scripts/CharacterGeneratorNPC.cs
@@ -10,12 +10,27 @@ public class CharacterGeneratorNPC : MonoBehaviour
     public VoiceList[] femaleVoices;
     public float randomVoicePitchRange = 0.1f;
     public AudioRandomizer voiceAudioSource;
+    public bool feminine;
+
     
     // Start is called before the first frame update
     void Start()
     {
+        feminine = false;
         GenerateCharacter();
+
     }
+
+
+    public void FemininePref()
+    {
+        feminine = true;
+    }
+    public void universalPref()
+    {
+        feminine = false;
+    }
+
 
 
     public void GenerateCharacter()
@@ -23,15 +38,22 @@ public class CharacterGeneratorNPC : MonoBehaviour
         Randomizer[] randomizers = GetComponentsInChildren<Randomizer>();
         foreach (Randomizer randomizer in randomizers)
         {
-            randomizer.GetRandomSprite();
+            randomizer.GetRandomSprite(feminine);
+        }
+        if (voiceAudioSource!= null)
+        {
+            // todo: switch array based on player preference
+            voiceAudioSource.clipSelection = femaleVoices[Random.Range(0, femaleVoices.Length)].voices;
+            voiceAudioSource.pitch = Random.Range(-randomVoicePitchRange, randomVoicePitchRange);
         }
         
-        // todo: switch array based on player preference
-        voiceAudioSource.clipSelection = femaleVoices[Random.Range(0, femaleVoices.Length)].voices;
-        voiceAudioSource.pitch = Random.Range(-randomVoicePitchRange, randomVoicePitchRange);
+      
     }
 
+ 
+
     // this is the only way to serialize a nested array. Stupid, I know, but alas
+
     [Serializable]
     public class VoiceList
     {
--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class Randomizer : MonoBehaviour
 {
     private SpriteRenderer rend;
-    public Sprite[] spriteList;
+    public Sprite[] universalspriteList;
+    public Sprite[] feminineList;
+
 
     public Color[] colorList;
+
     
     void Awake()
     {
         rend = GetComponent<SpriteRenderer>();
+      
     }
 
-    public void GetRandomSprite()
+ 
+
+    public void GetRandomSprite(bool preference)
     {
-        rend.sprite = spriteList[Random.Range(0, spriteList.Length)];
-        rend.color = colorList[Random.Range(0, colorList.Length)];
+        if(preference == true)
+        {
+            rend.sprite = feminineList[Random.Range(0, feminineList.Length)];
+            rend.color = colorList[Random.Range(0, colorList.Length)];
+        }
+        else
+        {
+            rend.sprite = universalspriteList[Random.Range(0, universalspriteList.Length)];
+            rend.color = colorList[Random.Range(0, colorList.Length)];
+        }
+        
+      
 
     }
 
