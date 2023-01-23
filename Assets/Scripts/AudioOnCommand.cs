b511d6ffcc34664655eb152f6ceb7466265aa501
Celtican <Celtican5@gmail.com>
2023-01-23 01:04:44 -0500

    Added a ton of sound effects

---
+++ Assets/Scripts/AudioOnCommand.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AudioOnCommand : MonoBehaviour
+{
+    public AudioController.Audio audio;
+    
+    // intended for animations/buttons
+
+    public void PlayAudio()
+    {
+        PlayAudio(audio);
+    }
+
+    public void PlayAudio(AudioController.Audio audio)
+    {
+        AudioController.Instance.PlayAudio(audio);
+    }
+}
